da7a8d5090bf45929d5a5d4145cdc9b4cd47637b
Adam Sajdak <adam.sajdak@gmail.com>
2018-11-16 06:00:49 -0800

    Add services project

---
+++ src/BandwidthMeterService/Meter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BandwidthMeterService
+{
+    public class Meter
+    {
+        public IReadOnlyCollection<int> GetMetrics()
+        {
+            var metrics = new List<int>();
+            var random = new Random();
+            const int n = 100;
+            for (var i = 0; i < n; i++)
+            {
+                metrics.Add(random.Next());
+            }
+            return metrics;
+        }
+    }
+}
--- src/ConsoleClient/Program.cs
+++ src/ConsoleClient/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BandwidthMeterService;
 
 namespace ConsoleClient
 {
@@ -10,6 +11,13 @@ namespace ConsoleClient
     {
         static void Main(string[] args)
         {
+            var meter = new Meter();
+            foreach (var metric in meter.GetMetrics())
+            {
+                Console.WriteLine(metric);
+            }
+
+            Console.ReadKey();
         }
     }
 }
